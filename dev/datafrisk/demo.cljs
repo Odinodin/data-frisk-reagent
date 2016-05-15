16f2a6940537ea53efed4c563a071a49c2b1e9ae
Odin Standal <odin@odinodin.com>
2016-05-15 08:20:22 +0200

    Improve demo

--- dev/datafrisk/demo.cljs
+++ dev/datafrisk/demo.cljs
@@ -4,24 +4,34 @@
 
 (enable-console-print!)
 
+(defn Animals [data]
+  [:div "Awesome animals"
+   [:ul
+    (map-indexed (fn [i {:keys [animal age]}] ^{:key i} [:li (str animal ", " age " years old")]) (:animals data))]])
+
 ;; Needed in order to keep track of the state in the data frisk shell
 (def some-atom (r/atom {:data-frisk {:visible? true}}))
 
 (defn mount-root []
-  (r/render
-    [d/DataFriskShell
-     some-atom
-     ;; List of arguments you want to visualize
-     {:data {:some-string "a"
-             :vector-with-map [1 2 3 3 {:a "a" :b "b"}]
-             :a-set #{1 2 3}
-             :a-map {:x "x" :y "y" :z [1 2 3 4]}
-             :a-list '(1 2 3)
-             :a-seq (seq [1 2])
-             :an-object (clj->js {:a "a"})
-             :this-is-a-very-long-keyword :g}}
-     {:a :b :c :d}]
-    (js/document.getElementById "app")))
+  (let [data {:animals '({:animal "Monkey", :age 2}
+                         {:animal "Giraffe", :age 4}
+                         {:animal "Zebra" :age 3})
+              :some-string "a"
+              :vector-with-map [1 2 3 3 {:a "a" :b "b"}]
+              :a-set #{1 2 3}
+              :a-map {:x "x" :y "y" :z [1 2 3 4]}
+              :a-seq (seq [1 2])
+              :an-object (clj->js {:a "a"})
+              :this-is-a-very-long-keyword :g}]
+
+    (r/render
+      [:div
+       [Animals data]
+       [d/DataFriskShell
+        some-atom
+        ;; List of arguments you want to visualize
+        data]]
+      (js/document.getElementById "app"))))
 
 (defn ^:export main []
   (mount-root))
